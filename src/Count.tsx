13316fc7e2bc958051e93298a38e6565d603b3ba
TinaGorna <kristina.gorbunova1997@gmail.com>
2021-05-11 09:39:42 +0300

    button increment changed

--- src/Count.tsx
+++ src/Count.tsx
@@ -1,35 +1,32 @@
-import React from "react";
+
 
 type CountPropsType = {
     show: number | string
     start: number
+    maxV: number
     addInc: () => void
     addReset: () => void
-    maxV: number
 }
 
-export function Count(props: CountPropsType) {
-    const btnIncrement = props.show === props.maxV ||
-    props.show === "Set" ||
-    props.show === "Error" ?
-        "Btn-increment=max" :
-        "Btn-increment"
+function Count(props: CountPropsType) {
 
-    const btnReset = props.show === props.start ||
-    props.show === "Set" ||
-    props.show === "Error" ?
-        "Btn-reset-start" : "Btn-reset"
+    const classNameBtnIncrement = props.show === props.maxV || props.show === 'Set' || props.show === 'Error' ? "Btn-increment-max" : "Btn-increment"
+    const classNameBtnReset = props.show === props.start || props.show === 'Set' || props.show === 'Error' ? "Btn-reset-start" : "Btn-reset"
 
     return (
+
         <div className="Btn-wrapper">
-            <div className={btnIncrement}>
+
+            <div className={classNameBtnIncrement}>
                 <button onClick={props.addInc}>inc</button>
             </div>
-            <div className={btnReset}>
+
+            <div className={classNameBtnReset}>
                 <button onClick={props.addReset}>reset</button>
             </div>
+
         </div>
-    )
+    );
 }
 
 export default Count;
